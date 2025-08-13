0b68a2717903faafc0bf2db861fe79d9f429d654
salado-jofel <saladojofel@gmail.com>
2025-08-13 22:38:26 +0800

    fix: changed name for db client

--- configs/db.js
+++ configs/db.js
@@ -6,8 +6,8 @@ console.log("Connecting to database...: ", process.env.CONNECTION_STRING_NEON);
 const db = new Client({
   host: process.env.BASE_URL,
   port: process.env.DB_PORT,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   // connectionString: process.env.CONNECTION_STRING_NEON,
   ssl: { rejectUnauthorized: false },
