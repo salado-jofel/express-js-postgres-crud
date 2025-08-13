cce65f7f60305b9f46fcdad8da5ebddf53a1b496
salado-jofel <saladojofel@gmail.com>
2025-08-13 15:42:44 +0800

    fix: changed port

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 import express from "express";
 import db from "./configs/db.js";
 import contactRouter from "./routes/contact-routes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
 db.connect()
-  .then(() => console.log("Connected to Supabase PostgreSQL"))
+  .then(() => console.log("Connected to Neon PostgreSQL"))
   .catch((err) => console.error("DB connection error:", err));
+console.log("MAIN PORT: ", process.env.PORT);
 
-app.listen(3000, () => {
-  console.log(`server is running... 3000`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use("/api/v1/contacts", contactRouter);
