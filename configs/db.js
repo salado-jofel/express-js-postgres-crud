c407842e5e0776148453cd35620027d905bb4b5c
salado-jofel <saladojofel@gmail.com>
2025-08-13 22:26:17 +0800

    fix: changed to supabase

--- configs/db.js
+++ configs/db.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 console.log("Connecting to database...: ", process.env.CONNECTION_STRING_NEON);
 const db = new Client({
-  //   host: process.env.BASE_URL,
-  //   port: process.env.DB_PORT,
-  //   user: process.env.USER,
-  //   password: process.env.PASSWORD,
-  //   database: process.env.DATABASE,
-  connectionString: process.env.CONNECTION_STRING_NEON,
+  host: process.env.BASE_URL,
+  port: process.env.DB_PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  // connectionString: process.env.CONNECTION_STRING_NEON,
   ssl: { rejectUnauthorized: false },
 });
 
